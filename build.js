ca0d506c79b04f39d352fd80ec38e823b96dc5a8
samuelgoto@gmail.com <samuelgoto@gmail.com>
2018-10-30 12:11:44 -0700

    adds a link to the binary release

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ let dataset = {
  "@type": "Layer",
  "name": "A car model overlay, based on the Stanford car dataset ported to JSON-LD",
  "description": "With this overlay you can get information about over 200 different car models!",
+ "download": "https://github.com/samuelgoto/cars/releases/download/0.0.1/cars.tar.gz",
  "items": [
  ]
 };
